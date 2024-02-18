5c4e40c48969e45463a8745c0d6e14921dc40ba1
Michal Ciberej <michal.ciberej@gmail.com>
2024-02-18 17:24:35 +0200

    Add constant utility styles

---
+++ constants/THEME.ts
@@ -0,0 +1,18 @@
+const COLORS = {
+  text: '#353535',
+  background: '#f8f8f8',
+  primary: '#fdb669',
+  secondary: '#d4aefc',
+  accent: '#fd63a6',
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export { COLORS, SIZES };
